18e2d5dc9c911df2cd3a2df35e9138d80dee65f5
Rae Gebhart <raegebhart@gmail.com>
2023-01-31 13:35:05 -0700

    feat: Add deleteIdea method to App comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react'
-import Ideas from '../Ideas/Ideas'
-import Form from '../Form/Form'
-import './App.css'
+import React, { Component } from "react";
+import Ideas from "../Ideas/Ideas";
+import Form from "../Form/Form";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       ideas: [
-        {id: 1, title: 'Fix linkedIn', description: 'Use career flow linked in optimizer'}
-      ] 
-    }
+        {
+          id: 1,
+          title: "Fix linkedIn",
+          description: "Use career flow linked in optimizer",
+        },
+      ],
+    };
   }
 
-  addIdea = newIdea => {
-    this.setState({ideas: [...this.state.ideas, newIdea]}) 
-  }
+  addIdea = (newIdea) => {
+    this.setState({ ideas: [...this.state.ideas, newIdea] });
+  };
 
-  render() { 
+  deleteIdea = (id) => {
+    const remainingIdeas = this.state.ideas.filter((idea) => idea.id != id);
+    this.setState({ ideas: remainingIdeas });
+  };
+
+  render() {
     return (
-      <main className='App-header'>
+      <main className="App-header">
         <h1>IdeaBox!</h1>
         <Form addIdea={this.addIdea} />
-        {!this.state.ideas.length && <h3>No Ideas Yet -- Why Don't We Make Some?</h3>}
-        <Ideas ideas={this.state.ideas} />
+        {!this.state.ideas.length && (
+          <h3>No Ideas Yet -- Why Don't We Make Some?</h3>
+        )}
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} />
       </main>
-    )
+    );
   }
 }
 
-export default App
+export default App;
