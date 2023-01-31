68bbd243dfa23e3c08afd42f5997af610132eb6f
Rae Gebhart <raegebhart@gmail.com>
2023-01-31 12:38:21 -0700

    feat: Add Card component to Ideas Component

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Ideas.css'
 
 const Ideas = () => {
   return (
-    <div>Ideas</div>
+    <div className="ideas-container">
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
