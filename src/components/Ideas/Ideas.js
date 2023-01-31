94462c23a4ed2a5186e6a11d92552b10020d3529
Rae Gebhart <raegebhart@gmail.com>
2023-01-31 13:36:19 -0700

    feat: Pass deleteIdea method down to Card comp

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import Card from '../Card/Card'
-import './Ideas.css'
+import React from "react";
+import Card from "../Card/Card";
+import "./Ideas.css";
 
-const Ideas = ({ ideas }) => {
-  const cards = ideas.map(idea => <Card id={idea.id} title={idea.title} description={idea.description} key={idea.id} />)
-  return (
-    <div className="ideas-container">
-      {cards}
-    </div>
-  )
-}
+const Ideas = ({ ideas, deleteIdea }) => {
+  const cards = ideas.map((idea) => (
+    <Card
+      id={idea.id}
+      title={idea.title}
+      description={idea.description}
+      key={idea.id}
+      deleteIdea={deleteIdea}
+    />
+  ));
+  return <div className="ideas-container">{cards}</div>;
+};
 
-export default Ideas
+export default Ideas;
