9738388cccabdc3db28ec170278cb21c1962c41d
Rae Gebhart <raegebhart@gmail.com>
2023-01-31 12:50:45 -0700

    feat: Iterate over state to display all cards

--- src/components/Ideas/Ideas.js
+++ src/components/Ideas/Ideas.js
@@ -2,12 +2,11 @@ import React from 'react'
 import Card from '../Card/Card'
 import './Ideas.css'
 
-const Ideas = () => {
+const Ideas = ({ ideas }) => {
+  const cards = ideas.map(idea => <Card id={idea.id} title={idea.title} description={idea.description} key={idea.id} />)
   return (
     <div className="ideas-container">
-      <Card />
-      <Card />
-      <Card />
+      {cards}
     </div>
   )
 }
