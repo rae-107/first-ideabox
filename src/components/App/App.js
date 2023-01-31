34b7c448216678e82ee4eb4a1ee928791965acca
Rae Gebhart <raegebhart@gmail.com>
2023-01-31 10:14:29 -0700

    feat: Add App class component

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      ideas: [
+
+      ] 
+    }
+  }
+
+  render() {
+    return (
+      <div className='App-header'>
+        <h1>IdeaBox!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
